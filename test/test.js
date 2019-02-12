34986ec9b3b1aa334997886955554edf1ee4bce4
Geir Gåsodden <geir.gasodden@pythonia.no>
2019-02-12 12:38:58 +0100

    Updates test (patch)

--- test/test.js
+++ test/test.js
@@ -1,11 +1,9 @@
 const test = require('ava')
-const pkg = require('../package.json')
-const dependencies = pkg.dependencies || {}
-const devDependencies = pkg.devDependencies || {}
+const { dependencies, devDependencies } = require('../package.json')
 const dropModules = ['react-json-view']
 const isDropped = module => !dropModules.includes(module)
 
-Object.keys(dependencies).forEach(dependency => {
+Object.keys(dependencies).filter(isDropped).forEach(dependency => {
   test(`${dependency} loads ok`, t => {
     const module = require(dependency)
     t.truthy(module)
